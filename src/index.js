da15d4c17f3b286376481bb1f9e5ccb1e1a779e2
charanchbuff <you@example.com>
2020-07-06 16:19:39 -0400

    Final Commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const task = require('./models/tasks')
 const taskRouter = require('../src/taskR')
 const userRouter = require('./userR')
 const auth = require('../src/middleware/auth')
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 const app = express()
 
 app.use(express.json())
@@ -14,16 +14,3 @@ app.use(userRouter)
 app.listen(port, () => {
     console.log('server up')
 })
-
-// const test = async () => {
-//     try{
-//         const users = await user.findById('5efaf3fc1c41723c70d3ee7d')
-//         await users.populate('taskd').execPopulate()
-//         console.log(users.taskd)
-//     }
-//     catch(e) {
-//         console.log('Error')
-//     }
-   
-// }
-// test()
